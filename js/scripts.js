43dc164146ad56e0970a41900bfb6de758f6adad
Emily <emily.cone.school@gmail.com>
2019-03-15 12:58:52 -0700

    Imported Basic Layout from Codepen

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@ console.log('OK');
 $('#gallery-1').lightGallery();
 
 // Your Awesome Scripts
+
+// Footer
+
+// Get current year
+var today = new Date();
+
+// Store current year
+var currYear = today.getFullYear();
+
+// Display current year
+$('.year').text(currYear);
