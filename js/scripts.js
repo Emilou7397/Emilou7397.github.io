4019e5f72d2132bc203f8fa0147b639039839677
Emily Cone <econe@eagles.ewu.edu>
2019-04-08 18:13:48 -0700

    More Formatting Updates
    Removed light gallery, mainly. I prefer to format my own display.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,6 @@
 // Everything OK?
 console.log('OK');
 
-// Gallery Animations
-
-$('a').mouseenter(function()	{
-    $(this).children('img').toggleClass('zoomed');
-});
-
-$('a').mouseleave(function()	{
-    $(this).children('img').toggleClass('zoomed');
-});
-
-// Light Gallery
-$('#gallery-1').lightGallery();
-
 // Your Awesome Scripts
 
 // Footer
