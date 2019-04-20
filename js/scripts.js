01a76a2bf68487a374521c32d53fdcca43a8fe23
Emily Cone <econe@eagles.ewu.edu>
2019-04-19 17:02:40 -0700

    Added Work Detail Overlay

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,18 @@ console.log('OK');
 
 // Your Awesome Scripts
 
+// Work detail overlay
+
+$("#gallery a").click(function(e) {
+    e.preventDefault();
+    $("#work-detail").addClass("visible");
+})
+
+$("#work-detail").click(function(e) {
+    e.preventDefault();
+    $("#work-detail").removeClass("visible");
+})
+
 // Footer
 
 var today = new Date();
