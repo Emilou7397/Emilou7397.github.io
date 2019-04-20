602ea0f8ac23801203ee5ef3eccc060d3b5c38e4
Emily Cone <econe@eagles.ewu.edu>
2019-04-20 12:28:19 -0700

    Gallery Filtering Added

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,31 @@ console.log('OK');
 
 // Your Awesome Scripts
 
+// Gallery Filtering
+
+$("button").click(function(e) {
+    e.preventDefault();
+    let filter = e.currentTarget.classList[0];
+    let pieces = $("#gallery").children("a");
+    let classes;
+    let flag = false;
+    $.each(pieces, function(piece) {
+        classes = pieces[piece].classList;
+        $.each(classes, function(clss) {
+            if (classes[clss] == filter){
+                flag = true;
+            }
+        })
+        if (!flag) {
+            pieces[piece].classList.add("hidden");
+        }
+        else {
+            pieces[piece].classList.remove("hidden");
+        }
+        flag = false;
+    });
+})
+
 // Work detail overlay
 
 $("#gallery a").click(function(e) {
